fa1a631677c31a32f5993dc3826063cb154ef676
Paulo Salvatore <salvatoregames@gmail.com>
2023-04-28 21:07:20 -0300

    Responsividade adicionada

--- src/App.jsx
+++ src/App.jsx
@@ -24,9 +24,11 @@ function App() {
   return (
     <>
       <h1>Samsung Ocean</h1>
-      {itens.map(function (item, index) {
-        return <Card item={item} key={`card_${index}`} />;
-      })}
+      <div className="cards">
+        {itens.map(function (item, index) {
+          return <Card item={item} key={`card_${index}`} />;
+        })}
+      </div>
     </>
   );
 }
